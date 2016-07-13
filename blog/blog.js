f109d13fe2097a832c829d2efdb5d9ecc5c66a51
Megan Curley <curley.megan@gmail.com>
2016-07-12 19:00:02 -0700

    Updates to blog like counter

--- blog/blog.js
+++ blog/blog.js
@@ -6,34 +6,31 @@ link.addEventListener("click", like);
 
 function like(event) {
   event.preventDefault();
-  // call the span with class "like-count"
-  var countSpan = document.querySelector(".like-count");
-  // access the text content within the span
-  var a = countSpan.textContent;
-  // define the text as a number vs a string
+  var likes = document.querySelector(".like-count");
+  var a = likes.textContent;
   var b = parseInt(a);
-  // add a value of 1 on click
   var c = b + 1;
-  // rewrite the text content within the span to reflect new value
-  countSpan.textContent = c;
+  likes.textContent = c;
 }
 
 
 function comment(event) {
   event.preventDefault();
-  // define the variable for the new-comment form
-  var body = document.querySelector("#new-comment-body");
-  // define the value of the body input
-  var entry = body.value;
-  // create the new div element
-  var commentBox = document.createElement("div");
-  // assign attributes to the new "div"
-  commentBox.setAttribute("class", "comment");
-  // define variable for comment display
-  var newComment = document.querySelector("#comments");
-  // display the text entry in a new block
-  commentBox.textContent = entry;
-  newComment.appendChild(commentBox);
+  // define the comment box text area as a var
+  // define the input as a var
+  // create the new div
+  // define the div class
+  // define the text content as the input var defined above
+  // append the div to a parent
+  // make the comment box value null
+
+  var textBox = document.querySelector("#new-comment-body");
+  var entry = textBox.value;
+  var commentsDiv = document.createElement("div");
+  commentsDiv.setAttribute("class","comment");
+  commentsDiv.textContent = entry;
+  var comments = document.querySelector("#comments");
+  comments.appendChild(commentsDiv);
   document.querySelector("#new-comment-body").value = null;
 
 }
