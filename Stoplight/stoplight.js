aa1c0d43123ab7fc0624df8af88b11e56136f15d
Megan Curley <curley.megan@gmail.com>
2016-07-18 19:36:44 -0700

    Stoplight homework

---
+++ Stoplight/stoplight.js
@@ -0,0 +1,45 @@
+var stopButton = document.querySelector(".stop-button");
+var slowButton = document.querySelector(".slow-button");
+var goButton = document.querySelector(".go-button");
+var cautionButton = document.querySelector(".caution-button");
+
+stopButton.addEventListener("click", lightUpRed);
+slowButton.addEventListener("click", lightUpYellow);
+goButton.addEventListener("click", lightUpGreen);
+cautionButton.addEventListener("click", flashYellow);
+
+var red = document.querySelector(".stop");
+var yellow = document.querySelector(".slow");
+var green = document.querySelector(".go");
+var flashing;
+
+function lightUpRed() {
+  red.classList.add("stop-light");
+  yellow.classList.remove("slow-light");
+  green.classList.remove("go-light");
+  clearInterval(flashing);
+}
+
+function lightUpYellow() {
+  yellow.classList.add("slow-light");
+  red.classList.remove("stop-light");
+  green.classList.remove("go-light");
+  clearInterval(flashing);
+}
+
+function lightUpGreen() {
+  green.classList.add("go-light");
+  red.classList.remove("stop-light");
+  yellow.classList.remove("slow-light");
+  clearInterval(flashing);
+}
+
+function flash() {
+  red.classList.remove("stop-light");
+  green.classList.remove("go-light");
+  yellow.classList.toggle("slow-light");
+}
+
+function flashYellow() {
+  flashing = setInterval(flash, 1000);
+}
