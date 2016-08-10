91356da1852c2a020cccf2b20934bfbea4e96d85
Megan Curley <curley.megan@gmail.com>
2016-08-09 19:29:07 -0700

    Eat Happy images
    added food icon images

---
+++ eathappy/eathappy.js
@@ -0,0 +1,17 @@
+$("#burn-button").on("click", loadResults);
+
+function loadResults() {
+  $("#food-screen").addClass("hidden");
+  $("#burn-screen").removeClass("hidden");
+};
+
+$(".food-icon").on("click", loadResults);
+
+function loadResults() {
+  $("#food-screen").addClass("hidden");
+  $("#burn-screen").removeClass("hidden");
+};
+
+// when apple is clicked
+// open overlay with button
+//
