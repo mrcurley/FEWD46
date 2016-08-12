abcbf54e361501371e903cd19a67ead70927d962
Megan Curley <curley.megan@gmail.com>
2016-08-12 15:33:50 -0700

    Updates
    Style updates, js additions

--- eathappy/eathappy.js
+++ eathappy/eathappy.js
@@ -27,13 +27,32 @@ function getItem(event) {
   $("#burn-button").on("click", getCalories);
 
   function getCalories () {
-    var bikeCal = (data/150/0.00632);
-    var walkCal = (data/150/0.048);
-    var runCal = (data/150/0.079);
+    $("#food-screen").addClass("hidden");
+    $("#burn-screen").removeClass("hidden");
+
+    $(".footer").addClass("results-footer");
+    $(".footer-text").text("");
+    $("#burn-button").text("PICK AGAIN");
+
+    var bikeCal = parseInt(data/150/0.00632);
+    var walkCal = parseInt(data/150/0.048);
+    var runCal = parseInt(data/150/0.079);
 
     $("#bike-total").text(bikeCal);
     $("#walk-total").text(walkCal);
     $("#run-total").text(runCal);
+
+    if (data == 0) {
+      var pepTalk = $(".pep-talk");
+      var feedback = $("<h3>").text("💁🏼 Drink up girl! Drinking plenty of water is a great way to stay hydrated and avoid overeating.");
+      feedback.appendTo(pepTalk);
+    }
+
+    else if (data == 95,226,91,124,40,275,65) {
+      var pepTalk = $(".pep-talk");
+      var feedback = $("<h3>").text("👏🏻👏🏻👏🏻 Excellent choice! Keep on fighting the good fight against donuts, cheeseburgers and bacon.");
+      feedback.appendTo(pepTalk);
+    }
   }
 }
 
