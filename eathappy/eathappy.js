c9febf69c998b7683697f8095041892e2c82a8cd
Megan Curley <curley.megan@gmail.com>
2016-08-16 18:42:20 -0700

    clean up
    clean up

--- eathappy/eathappy.js
+++ eathappy/eathappy.js
@@ -1,33 +1,23 @@
-//$("#burn-button").on("click", loadResults);
-
-//function loadResults() {
-//  $("#food-screen").addClass("hidden");
-//  $("#burn-screen").removeClass("hidden");
-//};
-
-//$(".food-icon").on("click", loadResults);
-
-//function loadResults() {
-//  $("#food-screen").addClass("hidden");
-//  $("#burn-screen").removeClass("hidden");
-//};
-
-
+// getItem
 // when apple is clicked
 // add class for active to the div
 // get data value of image and set as var
 
 $(".food-icon").on("click", getItem);
 
+// Get food item selection data
 function getItem(event) {
   event.preventDefault();
   var foodIcon = $(event.target).parents(".food-icon");
   var data = foodIcon.data("value");
+  var foodItem = foodIcon.attr("name");
+
   foodIcon.toggleClass("active");
   $(".footer").animate({
-    height: "100px", padding: "30px 0px 10px"
+    height: "10%", padding: "30px 0px 10px"
   });
 
+// Calculate Calories and load results
   $("#burn-button").on("click", getCalories);
 
   function getCalories () {
@@ -55,15 +45,29 @@ function getItem(event) {
       feedback.appendTo(pepTalk);
     }
   }
+
+// Restaurant Finder
+  $("#eat-now-button").on("click", findRestaurants);
+
+  function findRestaurants () {
+
+  }
+
 }
 
 
+// findRestaurants
+// When the eat now button is clicked
+// Get location
+// When location comes back Call Yelp api
+// Use food name & location to build url for api call
+// Hide Results screen
+// Show Restaurants screen
+// Append restaurants to restaurants list div
 
 
-
-
+// getCalories
 // when the burn button is clicked
-// parse int the data
 // run the formula to calculate minutes of walking, running, biking
 // add h1 to each results box div
 // assign the value of the formula
